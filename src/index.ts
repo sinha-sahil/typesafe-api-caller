eecc08b321c8e839deb16942c05ac920ac468385
Sahil Sinha <sahil.sinha@juspay.in>
2022-06-19 18:02:53 +0530

    Exported modules from package.json, types from index and updated package version

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export { APIFailure } from './types/APIFailure';
 export { APISuccess } from './types/APISuccess';
 export type { APIRequest } from './types/APIRequest';
 export type { APIResponse } from './types/APIResponse';
+export type { JSONObject } from './types/JSONObject';
+export type { HttpMethod } from './types/HttpMethod';
